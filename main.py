fe0bebd680472fc592585773010c5b35f01515a4
Seng-Pan <pseng4647@gmail.com>
2024-10-19 12:38:25 +0700

    updated ocr

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ import numpy as np
 from PIL import Image as Img
 import pytesseract as pyt
 import re
+from dateutil import parser
 
 
 pyt.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'  
 
 
-image_dir = "C:/Users/Seng Pan/Can_Do_Crew_AI_Project/Payment_ImageToText/src/KBZ/"
+image_dir = "C:/Users/Seng Pan/Can_Do_Crew_AI_Project/Payment_ImageToText/src/CB/"
 
 # Regular expression patterns for extracting fields
-date_pattern = re.compile(
-    r"(\d{1,2}[/-]\d{1,2}[/-]\d{2,4})|(\d{1,2} \w+ \d{4})|(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})"
-)
-amount_pattern = re.compile(r"-?\d{1,3}(?:,\d{3})*(?:\.\d{2})?\s?(MMK|Ks|Kyat)?")
-send_from_pattern = re.compile(r"(From|Sender Name|Send From)\s?:?\s?([A-Za-z\s]+)")
-send_to_pattern = re.compile(r"(To|Receiver Name|Send To)\s?:?\s?([A-Za-z\s]+)")
-notes_pattern = re.compile(r"(Notes|Purpose)\s?:?\s?(.+)")
-transaction_type_pattern = re.compile(r"\s?:?\s?Transfer")
+desired_date_format = "%Y/%m/%d"
+
+transtype_pattern = re.compile(r"(Transaction Type|Type)\s?:?\s?(.+)")
+notes_pattern = re.compile(r"^(Notes|Note|Purpose|Reason)\s?:?\s?(.+)")
+transtime_pattern = re.compile(r"^(Transaction Time|Date and Time|Date & Time|Transaction Date)\s?:?\s?(.+)")
+transno_pattern = re.compile(r"^(Transaction No|Transaction ID)\s?:?\s?(.+)")
+receiver_pattern = re.compile(r"(To|Receiver Name|Send To)\s?:?\s?([A-Za-z0-9\s]+)")
+sender_pattern = re.compile(r"^(From|Sender Name|Send From)\s?:?\s?(.+)")
+amount_data_pattern = re.compile(r"^(Amount|Total Amount)\s?:?\s?(.+)")
 
 def extract_text_from_image(image_path):
     """
@@ -52,62 +54,143 @@ def extract_text_from_image(image_path):
 
         # Apply a threshold to convert the image to binary (black and white)
         # Adjust the threshold value to ensure better extraction of gray text
-        _, thresh = cv2.threshold(sharpened, 150, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        _, thresh = cv2.threshold(sharpened, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        # Inpainting to remove the watermark
+        result = cv2.inpaint(img, thresh, inpaintRadius=3, flags=cv2.INPAINT_TELEA)
 
         # Convert back to a PIL image
         pil_image = Img.fromarray(thresh)
 
         # Use Tesseract to do OCR on the image
-        text = pyt.image_to_string(pil_image, lang='eng')
+        config = "--psm 6"
+        text = pyt.image_to_string(pil_image, config=config, lang='eng')
         return text
     
     except Exception as e:
         print(f"Error processing image {image_path}: {e}")
         return None
+    
+# Split text into lines
+def split_text_into_lines(text):
+    lines = text.split('\n')
+    return [line.strip() for line in lines if line.strip()]
+
+def extract_date_time(date_time_str):
+    """
+    Extracts date and time from the input string using regex and dateutil parser.
+
+    :param date_time_str: String containing date and time
+    :return: Tdate, time
+    """
+
+    # Define regular expressions to match different date and time formats
+    date_pattern = re.compile(r"(\d{1,2}[/-]\d{1,2}[/-]\d{2,4}|\d{1,2} \w+ \d{4}|\w+ \d{1,2}, \d{4})")
+    time_pattern = re.compile(r"\b((1[0-2]|0?[1-9]):[0-5][0-9](?::[0-5][0-9])?\s?[APap][Mm]|(2[0-3]|[01]?[0-9]):[0-5][0-9](?::[0-5][0-9])?)\b")  
+
+    try:
+          # Search date and time matches in the input string
+          date_match = date_pattern.search(date_time_str)
+          times_match = time_pattern.search(date_time_str) 
+    
+          # Parse the date part
+          try:
+              if date_match:
+                date_obj = parser.parse(date_match.group())
+                formatted_date = date_obj.strftime("%Y/%m/%d")
+              else:
+                formatted_date = ""
+          except:
+                formatted_date = ""
+          
+          # Parse the time part
+          try:
+              if times_match:
+                time_obj = parser.parse(times_match.group())
+                formatted_time = time_obj.strftime("%H:%M:%S")
+              else:
+                formatted_time = ""
+          except:
+                formatted_time = ""
+        
+    except Exception as e:
+           print(f"Error parsing date or time: {e}")
+
+    return formatted_date, formatted_time
+
+def extract_amount_only(amount_str):    
+    """
+    Extracts numeric amount from the amount string using regex.
+
+    :param amount_str: amount with negative sign, MMK, Ks
+    :return: numeric amount as a string
+    """
+
+    # formatted_amount = amount_str
+    amount_only_pattern = re.compile(r"-?\d*(?:,\d*)*(?:\.\d{2})?")
+    amount_pattern_match = amount_only_pattern.search(amount_str)
+    
+    if amount_pattern_match:
+        formatted_amount = amount_pattern_match.group().replace("-","").strip()
+        
+    return formatted_amount
 
 def extract_transaction_data(text):
     
     transaction_data = {
+        "Transaction No" : None,
+        "Date": None,
+        "Time": None,
+        "Transaction Time" : None,
         "Transaction Type": None,
         "Sender Name": None,
         "Amount": None,
         "Receiver Name": None,
-        "Date": None,
         "Notes": None
     }
 
-    # Use regular expressions to find key information
-    # Transaction Type
-    transaction_type_match = transaction_type_pattern.search(text)
-    if transaction_type_match:
-        transaction_data["Transaction Type"] = transaction_type_match.group(0).strip()
+    # Use regular expressions to find key information line by line
+    lines = split_text_into_lines(text)
+    for line in lines:
+        # Transaction Time
+        if re.search(transtime_pattern, line):
+            transtime_pattern_match = transtime_pattern.search(line)
+            date_time_str  = transtime_pattern_match.group(2).strip().strip('@').strip()
+            transaction_data["Transaction Time"] = date_time_str            
+            transaction_data["Date"], transaction_data["Time"] = extract_date_time(date_time_str)     
 
-    # Sender Name
-    sender_match = send_from_pattern.search(text)
-    if sender_match:
-        transaction_data["Sender Name"] = sender_match.group(2).strip()
-    else:
-        transaction_data["Sender Name"] = None  # Leave as null if not found
+        # Transaction No
+        elif re.search(transno_pattern, line):
+             transno_pattern_match = transno_pattern.search(line)
+             transaction_data["Transaction No"] = transno_pattern_match.group(2).strip().strip('@').strip()
 
-    # Amount
-    amount_match = amount_pattern.search(text)
-    if amount_match:
-        transaction_data["Amount"] = amount_match.group(0).replace("—", "-").strip()
+        # Transaction Type
+        elif re.search(transtype_pattern, line):
+             transtype_pattern_match = transtype_pattern.search(line)
+             transaction_type = transtype_pattern_match.group(2).strip().strip('@').strip()
+             # Remove non-alphanumeric characters except spaces
+             transaction_data["Transaction Type"] = re.sub(r"[^\w\s]", "", transaction_type)
 
-    # Receiver Name
-    receiver_match = send_to_pattern.search(text)
-    if receiver_match:
-        transaction_data["Receiver Name"] = receiver_match.group(2).strip()
+        # Amounts
+        elif re.search(amount_data_pattern, line):
+             amount_data_pattern_match = amount_data_pattern.search(line)
+             amount_string = amount_data_pattern_match.group(2).strip().strip('@').strip()
+             transaction_data["Amount"] = extract_amount_only(amount_string)
 
-    # Date
-    date_match = date_pattern.search(text)
-    if date_match:
-        transaction_data["Date"] = date_match.group(0).strip()
+        # Sender Name
+        elif re.search(sender_pattern, line):
+             sender_pattern_match = sender_pattern.search(line)
+             transaction_data["Sender Name"] = sender_pattern_match.group(2).strip().strip('@').strip()
 
-    # Notes
-    notes_match = notes_pattern.search(text)
-    if notes_match:
-        transaction_data["Notes"] = notes_match.group(2).strip()
+        # Receiver Name
+        elif re.search(receiver_pattern, line):
+             receiver_pattern_match = receiver_pattern.search(line)
+             transaction_data["Receiver Name"] = receiver_pattern_match.group(2).strip().strip('@').strip()
+
+        # Notes
+        elif re.search(notes_pattern, line):
+            notes_match = notes_pattern.search(line)
+            transaction_data["Notes"] = notes_match.group(2).strip()
 
     return transaction_data
 
@@ -125,11 +208,12 @@ for filename in os.listdir(image_dir):
 
             # Extract text using Tesseract
             extracted_text = extract_text_from_image(image_path)
-            print(f"Extracted data from {filename}: \n{extracted_text}\n")
+            print(f"Extracted data from {filename}")
 
             # Extract transaction information using regex
             transaction_info = extract_transaction_data(extracted_text)
-            transaction_info["File"] = filename  # Optional: Add filename for reference
+            print(transaction_info)
+            # transaction_info["File"] = filename  # Optional: Add filename for reference
 
             # Add to list of all transactions
             all_transactions.append(transaction_info)
